9c4e4298c0100f684a851bebbf0f34734894c507
AlyanTheCodingLegend <alyan.memon00@gmail.com>
2024-12-01 18:26:25 +0500

    Fixed indentation

--- app/_backend/_quizModule/_src/_types/questions.d.ts
+++ app/_backend/_quizModule/_src/_types/questions.d.ts
@@ -1,16 +1,16 @@
 export interface Question {
-    id: number;
-    topic: string;
-    difficulty: "Easy" | "Medium" | "Hard";
-    questionText: string;
-    options: string[];
-    correctAnswer: number; // Index of the correct option
-    explanation?: string;
-  }
-  
-  export interface Quiz {
-    topic: string;
-    difficulty: "Easy" | "Medium" | "Hard";
-    questions: Question[];
-  }
+  id: number;
+  topic: string;
+  difficulty: "Easy" | "Medium" | "Hard";
+  questionText: string;
+  options: string[];
+  correctAnswer: number; // Index of the correct option
+  explanation?: string;
+}
+
+export interface Quiz {
+  topic: string;
+  difficulty: "Easy" | "Medium" | "Hard";
+  questions: Question[];
+}
   
