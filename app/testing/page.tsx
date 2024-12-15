d667af457934e63d94758fbeec2a946ec099f4b5
Vainx056 <abdullahwaqar121105@gmail.com>
2024-12-15 07:17:51 +0500

    Using hashMap instead Arrays for Loading questions and Modified logic for shuffling

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -2,8 +2,14 @@
 
 import { useEffect, useState } from "react"
 
+type Value = {
+  topic: string,
+  difficulty: "Easy" | "Medium" | "Hard",
+  answer: number
+}
+
 export default function Page() {
-  const [value, setValue] = useState<number>(-1)
+  const [value, setValue] = useState<Value>({topic: '', difficulty: "Easy", answer: -1})
   const [render, setRender] = useState<number>(0)
 
   const handleSubmit = () => {
@@ -24,9 +30,11 @@ export default function Page() {
   }, [render])
 
     return (
-      <div>
-        <input type="number" className="text-black border-10" value={value} onChange={(e)=>setValue(Number(e.target.value))}/>
-        <button onClick={handleSubmit}>Click to submit answer</button>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <input type="number" className="text-black border-10" value={value.answer} onChange={(e)=>setValue(prev=>({...prev, answer: (Number(e.target.value))}))}/>
+        <input type="text" className="text-black" value={value.topic} onChange={(e)=>setValue(prev=>({...prev, topic: e.target.value}))}/>
+        <input type="text" className="text-black" value={value.difficulty} onChange={(e)=>setValue(prev=>({...prev, difficulty: e.target.value as "Easy" | "Medium" | "Hard"}))}/>
+        <button className="bg-red-700 text-white rounded-full" onClick={handleSubmit}>Click to submit answer</button>
       </div>
     )
 }
