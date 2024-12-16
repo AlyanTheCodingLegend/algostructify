adaee92bd109bb7e8b1172c3d3131170c62f23d8
Muhammad-Shah-zaib <MuhammdShahzaib@outlook.com>
2024-12-16 21:14:57 +0500

    Improved frontend for Mcqs Standard

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,4 +1,5 @@
 import { main } from "@/app/_backend/_quizModule/_src/app";
+import { ClientPageRoot } from "next/dist/client/components/client-page";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET() {
@@ -9,11 +10,11 @@ export function GET() {
 
 export async function POST(request: NextRequest){
     const req = await request.json()    
-    console.log(req)        
+    console.log("from server: ",req)        
 
-    await main(req.value.topic, req.value.difficulty, req.value.answer)
-    
-    return NextResponse.json({loggedIn: true})
+    const questions  =  await main(req.value.topic, req.value.difficulty, req.value.answer)
+    console.log("hello: ",questions);
+    return NextResponse.json(questions);
 }
 
 
