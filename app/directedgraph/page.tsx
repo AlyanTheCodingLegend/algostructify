919a5b47126308e66d48c74621473ad0436e9514
AlyanTheCodingLegend <alyan.memon00@gmail.com>
2024-12-25 00:27:54 +0500

    Visualized directed graph

---
+++ app/directedgraph/page.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { useRef, useState } from "react";
+import DirectedGraph from "../_datastructures/DirectedGraph";
+import Vertex from "./Vertex";
+
+export default function Page() {
+    const [graph] = useState(new DirectedGraph(5));
+    const [renderTrigger, setRenderTrigger] = useState(0);
+    const [insertVal, setInsertVal] = useState("");
+    const [insertEdgeVal, setInsertEdgeVal] = useState({start: "", end: ""});
+
+    const svgRef = useRef<SVGSVGElement>(null);
+
+    const forceRender = () => setRenderTrigger(prev=>prev+1);
+
+    const insertVertex = (name: string) => {
+        graph.addVertex(name);
+        forceRender();
+    }
+
+    const insertEdge = (startVertex: string, endVertex: string) => {
+        console.log(startVertex, endVertex)
+        graph.addEdge(startVertex, endVertex);
+        forceRender();
+    }
+
+    const renderGraph = () => {
+        if (!graph.matrix) {
+            return <div>No vertices in the directed graph!</div>
+        }
+
+        return graph.matrix.map((row, i) => {
+            const adjMatrix = row;
+            const name = graph.vertexNames[i];
+            return (
+                <Vertex index={i} name={name} adjMatrix={adjMatrix} svgRef={svgRef} renderTrigger={renderTrigger}/>
+            );
+        });
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-screen">
+            <h1 className="text-4xl font-semibold text-center">
+                Directed Graph
+            </h1>
+            <div className="flex items-center z-10 justify-center space-x-4">
+                <div
+                    className="px-4 py-2 bg-green-500 h-20 w-32 text-white rounded-md shadow-md flex flex-col"
+                >
+                    <input className="text-black" value={insertVal} onChange={(event)=>setInsertVal(event.target.value)} />
+                    <button onClick={() => insertVertex(insertVal)}>Insert Vertex</button>
+                </div>
+                <div
+                    className="px-4 py-2 bg-green-500 h-20 w-32 text-white rounded-md shadow-md flex flex-col"
+                >
+                    Start: 
+                    <select key="start" className="text-black" onChange={(event)=>{console.log(event.target.value);setInsertEdgeVal({...insertEdgeVal, start: event.target.value})}}>
+                        {graph.vertexNames.map((name) => (
+                            <option key={name} value={name}>{name}</option>
+                        ))}
+                    </select>
+                    End: 
+                    <select key="end" className="text-black" onChange={(event)=>setInsertEdgeVal({...insertEdgeVal, end: event.target.value})}>
+                        {graph.vertexNames.map((name) => (
+                            <option key={name} value={name}>{name}</option>
+                        ))}
+                    </select>
+                    <button onClick={() => insertEdge(insertEdgeVal.start, insertEdgeVal.end)}>Insert Edge</button>
+                </div>
+                {/* <div
+                    className="px-4 py-2 bg-red-500 h-20 w-32 text-white rounded-md shadow-md flex flex-col"
+                >
+                    <input className="text-black" value={deleteLeftVal} onChange={(event)=>setDeleteLeftVal(Number(event.target.value))} />
+                    <button onClick={() => deleteNodeLeft(deleteLeftVal)}>Delete Left Node</button>
+                </div>
+                <div
+                    className="px-4 py-2 bg-red-500 h-20 w-32 text-white rounded-md shadow-md flex flex-col"
+                >
+                    <input className="text-black" value={deleteRightVal} onChange={(event)=>setDeleteRightVal(Number(event.target.value))} />
+                    <button onClick={() => deleteNodeRight(deleteRightVal)}>Delete Right Node</button>
+                </div>
+                <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-md shadow-md"
+                    onClick={() => preOrderTraversal()}
+                    disabled={traversalInProcess}
+                >
+                    Pre-order Traversal
+                </button>
+                <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-md shadow-md"
+                    onClick={() => inOrderTraversal()}
+                    disabled={traversalInProcess}
+                >
+                    In-order Traversal
+                </button>
+                <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-md shadow-md"
+                    onClick={() => postOrderTraversal()}
+                    disabled={traversalInProcess}
+                >
+                    Post-order Traversal
+                </button> */}
+                
+            </div>
+            <div className="flex items-center justify-center w-screen h-screen overflow-scroll">
+                {renderGraph()}
+                <svg ref={svgRef} xmlns="http://www.w3.org/2000/svg" style={{ position: "absolute", width: "100%", height: "100%", pointerEvents: "none", zIndex: -1, top: 0, left: 0 }}>
+                    <defs>
+                        <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="0" refY="3.5" orient="auto">
+                            <polygon points="0 0, 10 3.5, 0 7" />
+                        </marker>
+                    </defs>
+                </svg>
+            </div>
+        </div>
+    );
+}
